b147fd6c6584e40c7068b14c944186dd64d005f9
Gaurang Manjrekar <gaurangmanjrekar@CIMUM002002.local>
2022-07-11 15:54:53 +0530

    + ui fixes

--- src/components/Covid.tsx
+++ src/components/Covid.tsx
@@ -13,8 +13,8 @@ const Covid: FunctionComponent<{}> = () => {
     <>
       {data.covidData && (
         <div className="flex flex-col w-full relative justify-end mt-8 md:mt-0 md:ml-2 ml-0">
-          <div className="h-[320px] w-full border-2 border-stone-300 rounded-3xl drop-shadow-lg bg-white">
-            <div className="h-[320px] flex flex-col justify-start">
+          <div className="h-[250px] w-full border-2 border-stone-300 rounded-3xl drop-shadow-lg bg-white">
+            <div className="h-[250px] flex flex-col justify-start">
               <img className="pt-4 h-12 w-12 mx-auto" src={corona_icon}></img>
               <p className="pt-2 text-[#DF7D76] text-xl text-xl mx-auto font-mont font-bold">
                 COVID UPDATES
@@ -74,7 +74,7 @@ const Covid: FunctionComponent<{}> = () => {
               )}
             </div>
           </div>
-          <div className="h-[324px] w-full rounded-3xl absolute">
+          <div className="h-[254px] w-full rounded-3xl absolute">
             <img className="mx-auto z-40" src={element}></img>
             {data.covidDataTimeSeriese && (
               <img
