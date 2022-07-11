37abb4e0a4d99d05674f7f2d671a0a6d927ca9d9
Gaurang Manjrekar <gaurangmanjrekar@CIMUM002002.local>
2022-07-11 19:04:01 +0530

    + footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,15 @@ import React, { FunctionComponent } from "react";
 
 const Footer: FunctionComponent<{}> = () => {
   return (
-    <div className="flex justify-center w-full h-20 bg-black">
-      <p className="font-mont pt-2 text-2xl text-white">Place Kaleidoscope</p>
+    <div className="flex flex-col justify-center w-full h-fit bg-black">
+      {/* <p className="font-mont pt-2 text-2xl text-white mx-auto">Kaleidoscope</p>
+      <p className="font-mont py-1 text-lg text-white font-bold mx-auto">
+        Cityscape
+      </p> */}
+      <p className="font-mont text-white text-lg mx-auto pt-8">Project By</p>
+      <p className="font-mont text-white text-xs mx-auto py-4">
+        Abhishek Dwivedi | Sumit Sarkar | Yashraj Chawathey | Gaurang Manjrekar
+      </p>
     </div>
   );
 };
